7012ec7f1e3365dbe04f713d023dc82095177f46
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-08-23 15:11:10 +0400

    js enter

---
+++ JavaScript/VanillaJS/lesson2-datatypes/js/script.js
@@ -0,0 +1,46 @@
+// Data Types
+// ================================================================================
+// Primitive data types
+// string 
+// var a = "Hello";
+// console.log(a);
+
+// number
+// var infoSt = 5;
+// var infoNd = 10;
+// console.log(infoSt+infoNd);
+
+// boolean
+// true
+// false
+// if (false) {
+//     console.log("This is true");
+// }else{
+//     console.log("This is false");
+// }
+
+// ================================================================================
+// Composite data types
+// array
+// const infoArr = ['hello',5,true];
+// console.log(infoArr);
+// object
+// const infoObj = {
+//     name:'bmw',
+//     model:'x5',
+//     color:'black',
+//     year:1990,
+//     newcar:false
+// }
+
+// console.log(infoObj.newcar);
+
+// ================================================================================
+// Special data types
+// undefined
+// const a = undefined;
+// console.log(a);
+// null
+// const b = null;
+// console.log(b);
+// ================================================================================
