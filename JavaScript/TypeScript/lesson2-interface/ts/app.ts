c5665bff7b3b4ab59deeb51eb80765c4ad37b2f4
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-12-08 17:28:51 +0400

    ts

---
+++ JavaScript/TypeScript/lesson2-interface/ts/app.ts
@@ -0,0 +1,63 @@
+// interface IDatabase{
+//     add();
+//     get();
+//     update();
+//     delete();
+// }
+
+// class MySql implements IDatabase{
+//     add(){
+//         console.log('MySql added');
+        
+//     }
+//     get(){
+//         console.log('MySql get');
+        
+//     }
+//     update(){
+//        console.log('MySql update');
+       
+//     }
+//     delete(){
+//        console.log('MySql delete');
+       
+//     }
+// }
+
+
+// class MongoDB implements IDatabase{
+//     add(){
+//         console.log('MongoDB added');
+        
+//     }
+//     get(){
+//         console.log('MongoDB get');
+        
+//     }
+//     update(){
+//        console.log('MongoDB update');
+       
+//     }
+//     delete(){
+//        console.log('MongoDB delete');
+       
+//     }
+// }
+
+// function AddDataBase(dt:IDatabase){
+//     dt.add();
+// }
+
+// function DeleteDataBase(dt:IDatabase){
+//     dt.delete();
+// }
+// AddDataBase(new MongoDB);
+// DeleteDataBase(new MySql);
+
+
+// var car:string = 'red';
+// var colorCar = (color:'blue'| 'green' | 'red' | 'orange')=>{
+//     car = color;
+// }
+// colorCar('orange')
+// console.log(car);
