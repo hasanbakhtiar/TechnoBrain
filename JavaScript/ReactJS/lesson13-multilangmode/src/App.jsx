36c2b814755ecf7cbf8f252ef4f9f776475666dd
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-01-10 16:22:03 +0400

    multilang

--- JavaScript/ReactJS/lesson13-multilangmode/src/App.jsx
+++ JavaScript/ReactJS/lesson13-multilangmode/src/App.jsx
@@ -3,17 +3,39 @@ import ModeTop from './components/ModeTop'
 import ModeBtn from './components/ModeBtn'
 import { useState } from 'react'
 import { useEffect } from 'react'
-
+import './i18n/i18next';
+import { useTranslation } from 'react-i18next'
 const App = () => {
-    const [mode,setMode] = useState('light');
+    const [mode,setMode] = useState(localStorage.getItem('mode'));
     useEffect(()=>{
+        localStorage.setItem('mode',mode)
         document.body.className = mode;
     },[mode])
+
+    const {t, i18n} = useTranslation();
+    const handleClick=(lang)=>{
+        i18n.changeLanguage(lang);
+    }
   return (
+    <>
+    
     <div className='container mt-5'>
         <ModeTop color={mode}/>
         <ModeBtn  color={mode} setColor={setMode}/>
     </div>
+
+
+    <div className="container mt-5">
+        <button className='btn btn-primary me-3' onClick={()=>{handleClick('az')}}>az</button>
+        <button className='btn btn-primary me-3' onClick={()=>{handleClick('en')}}>en</button>
+        <button className='btn btn-primary me-3' onClick={()=>{handleClick('sp')}}>sp</button>
+        <h1 className='mt-3'>{t('first.0')}</h1>
+    </div>
+
+    
+    
+    </>
+   
   )
 }
 
--- JavaScript/ReactJS/lesson13-multilangmode/src/index.jsx
+++ JavaScript/ReactJS/lesson13-multilangmode/src/index.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import './sass/style.css'
 import App from './App';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+
+ReactDOM.render(
+  <Suspense fallback={(<div>Loading...</div>)}><App/></Suspense>
+,document.querySelector('#root'));
 
